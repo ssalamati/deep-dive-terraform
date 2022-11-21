0d1abf37ee223669cf81bf71b1312d9d363ad4ea
Mohammad <salamati9819@gmail.com>
2022-11-20 18:14:18 -0800

    Add security groups

---
+++ security-groups.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "allow-ssh" {
+    name = "allow-ssh-sg"
+    vpc_id = aws_vpc.main.id
+    ingress {
+        cidr_blocks = [
+            "0.0.0.0/0"
+        ]
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+    }
+}
+
+resource "aws_security_group" "allow-tcp" {
+    name = "allow-tcp-sg"
+    vpc_id = aws_vpc.main.id
+    ingress {
+        cidr_blocks = [
+            "0.0.0.0/0"
+        ]
+        from_port = 8081
+        to_port = 8081
+        protocol = "tcp"
+    }
+}
+
+resource "aws_security_group" "egress-all" {
+    name = "egress-all-sg"
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
