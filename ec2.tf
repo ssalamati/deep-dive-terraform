23a3701588d7dac7083eeba35eb7691d96ee3349
Mohammad <salamati9819@gmail.com>
2022-11-20 19:34:10 -0800

    Add ec2 private

--- ec2.tf
+++ ec2.tf
@@ -5,12 +5,27 @@ resource "aws_instance" "ubuntu-server" {
   subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = true
 
-  key_name = "CYBERSECURITY_EC2_PUB"
-
+  key_name                    = "CYBERSECURITY_EC2_PUB"
 
   vpc_security_group_ids      = [aws_security_group.allow-ssh.id, aws_security_group.allow-tcp.id, aws_security_group.egress-all.id]
   
   tags = {
     Name = "linux-vm"
+  }
+}
+
+resource "aws_instance" "ubuntu-server-private" {
+  ami                         = data.aws_ami.ubuntu-1804.id
+  instance_type               = "t2.micro"
+
+  subnet_id                   = aws_subnet.private.id
+  associate_public_ip_address = false
+
+  key_name                    = "CYBERSECURITY_EC2_PRIV"
+
+  vpc_security_group_ids      = [aws_security_group.allow-ssh.id, aws_security_group.egress-all.id]
+  
+  tags = {
+    Name = "linux-vm-private"
   }
 }
